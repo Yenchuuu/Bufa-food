5a68ee186da69ac9ce2f9c147520b06b144e922b
Yenchuuu <shin180555@gmail.com>
2022-11-18 14:50:55 +0800

    chore: revise .env.example file

--- app.js
+++ app.js
@@ -4,27 +4,21 @@ const path = require('path')
 const axios = require('axios')
 const moment = require('moment')
 const { PORT, API_VERSION } = process.env
-// const indexRouter = require('./routes/index')
-// const usersRouter = require('./routes/users')
 
 const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', express.static(path.join(__dirname, 'views')))
 
-// app.use('/', indexRouter)
-// app.use('/users', usersRouter);
-
 /* Build API route */
 app.use('/api/' + API_VERSION, [require('./routes/food_route'), require('./routes/user_route')])
 
-// catch 404 and forward to error handler
+// // catch 404 and forward to error handler
 // app.use(function (req, res, next) {
 //   next(createError(404))
 // })
