8baf116ad165f0391176a3f27af1c3af5709bfab
Yenchuuu <shin180555@gmail.com>
2022-12-04 19:56:07 +0800

    fix: sql query of update cronjob

--- utils/setGoalsNight.js
+++ utils/setGoalsNight.js
@@ -7,7 +7,7 @@ async function crontab() {
     const date = moment().format('YYYY-MM-DD')
     const [users] = await db.execute('SELECT user_id, goal_calories, goal_carbs, goal_protein, goal_fat FROM `user_bodyInfo`')
     for (let userInfo of users) {
-      await db.execute('UPDATE `user_goal` SET goal_calories = ?, goal_carbs = ?, goal_protein = ?, goal_fat = ?, date = ? WHERE user_id = ?;', [userInfo.goal_calories, userInfo.goal_carbs, userInfo.goal_protein, userInfo.goal_fat, date, userInfo.user_id])
+      await db.execute('UPDATE `user_goal` SET goal_calories = ?, goal_carbs = ?, goal_protein = ?, goal_fat = ? WHERE user_id = ? AND date = ?;', [userInfo.goal_calories, userInfo.goal_carbs, userInfo.goal_protein, userInfo.goal_fat, userInfo.user_id, date])
     }
     console.log(`User goal on ${date} updated.`)
     // TODO: 小賴說這種寫法不好
