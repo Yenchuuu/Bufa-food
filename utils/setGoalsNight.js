0b1a50f88d5c76aaf578358bedfbebfbe56591a3
Yenchuuu <shin180555@gmail.com>
2022-12-04 17:36:24 +0800

    chore: adjust goal records crontab schedule to twice a day

---
+++ utils/setGoalsNight.js
@@ -0,0 +1,20 @@
+const moment = require('moment')
+require('dotenv').config()
+const db = require('./mysqlconf')
+
+async function crontab() {
+  try {
+    const date = moment().format('YYYY-MM-DD')
+    const [users] = await db.execute('SELECT user_id, goal_calories, goal_carbs, goal_protein, goal_fat FROM `user_bodyInfo`')
+    for (let userInfo of users) {
+      await db.execute('UPDATE `user_goal` SET goal_calories = ?, goal_carbs = ?, goal_protein = ?, goal_fat = ?, date = ? WHERE user_id = ?;', [userInfo.goal_calories, userInfo.goal_carbs, userInfo.goal_protein, userInfo.goal_fat, date, userInfo.user_id])
+    }
+    console.log(`User goal on ${date} updated.`)
+    // TODO: 小賴說這種寫法不好
+    process.exit()
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+crontab()
